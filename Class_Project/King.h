df8468b32eb0ebea7e472817130c0e1adee7545a
PritulaAlexey <pritula.alex91@mail.ru>
2023-12-11 16:14:56 +0700

    func check_temp() moved to private

--- Class_Project/King.h
+++ Class_Project/King.h
@@ -9,12 +9,12 @@ private:
 	string piece_name;
 	int x;
 	int y;
+	int check_Temp(int x, int y, vector<vector<int>> all_pieces_place, bool* any_one) const override;
 public:
 	King(string new_name, int new_x, int new_y);
 
 	void piece_Drawer() const override;
 	void check_Capture(vector<vector<int>> all_pieces_place) const override;
 	void insideGotoXY(short y, short x) const override;
-	int check_Temp(int x, int y, vector<vector<int>> all_pieces_place, bool* any_one) const override;
 };
 
--- Class_Project/Knight.h
+++ Class_Project/Knight.h
@@ -8,11 +8,11 @@ private:
 	string piece_name;
 	int x;
 	int y;
+	int check_Temp(int x, int y, vector<vector<int>> all_pieces_place, bool* any_one) const override;
 public:
 	Knight(string new_name, int new_x, int new_y);
 
 	void insideGotoXY(short y, short x) const override;
 	void piece_Drawer() const override;
 	void check_Capture(vector<vector<int>> all_pieces_place) const override;
-	int check_Temp(int x, int y, vector<vector<int>> all_pieces_place, bool* any_one) const override;
 };
